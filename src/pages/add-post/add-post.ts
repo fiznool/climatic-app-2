390aceb2365571a24d0bbf1885b6de02fe879266
Tom Spencer <fiznool@gmail.com>
2016-10-13 14:26:56 +0100

    Added form elements

--- src/pages/add-post/add-post.ts
+++ src/pages/add-post/add-post.ts
@@ -2,16 +2,23 @@ import { Component } from '@angular/core';
 
 import { NavParams, ViewController } from 'ionic-angular';
 
+interface Post {
+  title?: string,
+  description?: string
+}
+
 @Component({
   templateUrl: 'add-post.html'
 })
 export class AddPostPage {
+  private model: Post = {};
+
   constructor(
     private params: NavParams,
     private viewCtrl: ViewController
   ) {}
 
-  save(): void {
+  submit(): void {
     this.viewCtrl.dismiss({
       saved: true
     });
